4678585e21728429bdc2a83c7dcb6841291d9a96
Joko <soundless1200@gmail.com>
2026-03-28 08:56:28 +0700

    Xóa directory AbaloneAge khỏi branch main2

--- AbaloneAge/src/features/feature_engineering.py
+++
@@ -1,8 +0,0 @@
-"""Xu ly dac trung cho mo hinh."""
-
-import pandas as pd
-
-
-def build_features(df: pd.DataFrame) -> pd.DataFrame:
-    """Noi dung feature engineering se duoc bo sung tai day."""
-    return df.copy()
